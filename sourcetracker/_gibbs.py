866eb2b7455715b052635629931ae2d647f23682
andy6a <128522580+andy6a@users.noreply.github.com>
2023-05-17 14:52:29 -0700

    Update _gibbs.py with graphing

--- sourcetracker/_gibbs.py
+++ sourcetracker/_gibbs.py
@@ -10,114 +10,164 @@
 
 from __future__ import division
 
-import pandas as pd
-from biom import Table
-from sourcetracker._sourcetracker import (intersect_and_sort_samples,
+import os
+import click
+import numpy as np
+import pandas
+import matplotlib.pyplot as plt
+
+from biom import load_table
+from sourcetracker._cli import cli
+from sourcetracker._sourcetracker import (gibbs, intersect_and_sort_samples,
                                           get_samples, collapse_source_data,
                                           subsample_dataframe,
                                           validate_gibbs_input)
-from sourcetracker._sourcetracker import gibbs as _gibbs
-from sourcetracker._util import biom_to_df
-# import default values
-from sourcetracker._gibbs_defaults import (DEFAULT_ALPH1, DEFAULT_ALPH2,
-                                           DEFAULT_TEN, DEFAULT_ONE,
-                                           DEFAULT_HUND, DEFAULT_THOUS,
-                                           DEFAULT_FLS, DEFAULT_SNK,
-                                           DEFAULT_SRS, DEFAULT_SRS2,
-                                           DEFAULT_CAT, DEFAULT_TRU)
+
+from sourcetracker._util import parse_sample_metadata, biom_to_df
+from sourcetracker._plot import plot_heatmap
+from sourcetracker._plot import ST_graphs
 
 
-def gibbs(feature_table: Table,
-          sample_metadata: pd.DataFrame,
-          loo: bool = DEFAULT_FLS,
-          jobs: int = DEFAULT_ONE,
-          alpha1: float = DEFAULT_ALPH1,
-          alpha2: float = DEFAULT_ALPH2,
-          beta: float = DEFAULT_TEN,
-          source_rarefaction_depth: int = DEFAULT_THOUS,
-          sink_rarefaction_depth: int = DEFAULT_THOUS,
-          restarts: int = DEFAULT_TEN,
-          draws_per_restart: int = DEFAULT_ONE,
-          burnin: int = DEFAULT_HUND,
-          delay: int = DEFAULT_ONE,
-          per_sink_feature_assignments: bool = DEFAULT_TRU,
-          sample_with_replacement: bool = DEFAULT_FLS,
-          source_sink_column: str = DEFAULT_SNK,
-          source_column_value: str = DEFAULT_SRS,
-          sink_column_value: str = DEFAULT_SRS2,
-          source_category_column: str = DEFAULT_CAT)\
-              -> (pd.DataFrame, pd.DataFrame, Table, pd.DataFrame):
-    # convert tables
-    feature_table = biom_to_df(feature_table)
-    sample_metadata = sample_metadata.to_dataframe()
-    # run the gibbs sampler helper function (same used for q2)
-    results = gibbs_helper(feature_table, sample_metadata, loo, jobs,
-                           alpha1, alpha2, beta, source_rarefaction_depth,
-                           sink_rarefaction_depth, restarts, draws_per_restart,
-                           burnin, delay, per_sink_feature_assignments,
-                           sample_with_replacement, source_sink_column,
-                           source_column_value, sink_column_value,
-                           source_category_column)
-    # get the results (with fas)
-    # here we only return the three df (via q2)
-    mpm, mps, fas = results
-    # make list filter
+@cli.command(name='gibbs')
+@click.option('-i', '--table_fp', required=True,
+              type=click.Path(exists=True, dir_okay=False, readable=True),
+              help='Path to input BIOM table.')
+@click.option('-m', '--mapping_fp', required=True,
+              type=click.Path(exists=True, dir_okay=False, readable=True),
+              help='Path to sample metadata mapping file.')
+@click.option('-o', '--output_dir', required=True,
+              type=click.Path(exists=False, dir_okay=True, file_okay=False,
+                              writable=True),
+              help='Path to the output directory to be created.')
+@click.option('--loo', required=False, default=False, is_flag=True,
+              show_default=True,
+              help=('Classify each sample in `sources` using a leave-one-out '
+                    'strategy. Replicates -s option in Knights et al. '
+                    'sourcetracker.'))
+@click.option('--jobs', required=False, default=1,
+              type=click.INT, show_default=True,
+              help='Number of processes to launch.')
+@click.option('--alpha1', required=False, default=.001,
+              type=click.FLOAT, show_default=True,
+              help=('Prior counts of each feature in the training '
+                    'environments. Higher values decrease the trust in the '
+                    'training environments, and make the source environment '
+                    'distributions over taxa smoother. A value of 0.001 '
+                    'indicates reasonably high trust in all source '
+                    'environments, even those with few training sequences. A '
+                    'more conservative value would be 0.01.'))
+@click.option('--alpha2', required=False, default=.1,
+              type=click.FLOAT, show_default=True,
+              help=('Prior counts of each feature in the `unknown` environment'
+                    ' as a fraction of the counts of the current sink being '
+                    'evaluated. Higher values make the `unknown` environment '
+                    'smoother and less prone to overfitting given a training '
+                    'sample.'))
+@click.option('--beta', required=False, default=10,
+              type=click.FLOAT, show_default=True,
+              help=('Count to be added to each feature in each environment, '
+                    'including `unknown` for `p_v` calculations.'))
+@click.option('--source_rarefaction_depth', required=False, default=1000,
+              type=click.IntRange(min=0, max=None), show_default=True,
+              help=('Depth at which to rarify sources. If 0, no '
+                    'rarefaction performed.'))
+@click.option('--sink_rarefaction_depth', required=False, default=1000,
+              type=click.IntRange(min=0, max=None), show_default=True,
+              help=('Depth at which to rarify sinks. If 0, no '
+                    'rarefaction performed.'))
+@click.option('--restarts', required=False, default=10,
+              type=click.INT, show_default=True,
+              help=('Number of independent Markov chains to grow. '
+                    '`draws_per_restart` * `restarts` gives the number of '
+                    'samplings of the mixing proportions that will be '
+                    'generated.'))
+@click.option('--draws_per_restart', required=False, default=1,
+              type=click.INT, show_default=True,
+              help=('Number of times to sample the state of the Markov chain '
+                    'for each independent chain grown.'))
+@click.option('--burnin', required=False, default=100,
+              type=click.INT, show_default=True,
+              help=('Number of passes (withdarawal and reassignment of every '
+                    'sequence in the sink) that will be made before a sample '
+                    '(draw) will be taken. Higher values allow more '
+                    'convergence towards the true distribtion before draws '
+                    'are taken.'))
+@click.option('--delay', required=False, default=1,
+              type=click.INT, show_default=True,
+              help=('Number passes between each sampling (draw) of the '
+                    'Markov chain. Once the burnin passes have been made, a '
+                    'sample will be taken, and then taken again every `delay` '
+                    'number of passes. This is also known as `thinning`. '
+                    'Thinning helps reduce the impact of correlation between '
+                    'adjacent states of the Markov chain.'))
+@click.option('--per_sink_feature_assignments', required=False, default=False,
+              is_flag=True, show_default=True,
+              help=('If True, this option will cause SourceTracker2 to write '
+                    'out a feature table for each sink (or source if `--loo` '
+                    'is passed). These feature tables contain the specific '
+                    'sequences that contributed to a sink from a given '
+                    'source. This option can be memory intensive if there are '
+                    'a large number of features.'))
+@click.option('--sample_with_replacement', required=False,
+              default=False, show_default=True, is_flag=True,
+              help=('Sample with replacement instead of '
+                    'sample without replacement'))
+@click.option('--source_sink_column', required=False, default='SourceSink',
+              type=click.STRING, show_default=True,
+              help=('Sample metadata column indicating which samples should be'
+                    ' treated as sources and which as sinks.'))
+@click.option('--source_column_value', required=False, default='source',
+              type=click.STRING, show_default=True,
+              help=('Value in source_sink_column indicating which samples '
+                    'should be treated as sources.'))
+@click.option('--sink_column_value', required=False, default='sink',
+              type=click.STRING, show_default=True,
+              help=('Value in source_sink_column indicating which samples '
+                    'should be treated as sinks.'))
+@click.option('--source_category_column', required=False, default='Env',
+              type=click.STRING, show_default=True,
+              help=('Sample metadata column indicating the type of each '
+                    'source sample.'))
+##(added click option for stats function)
+@click.option('--diagnostics', required=False, default=False, is_flag=True,
+              show_default=True)
+@click.option('--limit', required=False, default=0.05, type=click.FLOAT, 
+              show_default=True)
+##(added options for graphical ouput and varying stats functions)
+@click.option('--stacked_bar', required=False, default=False,
+              show_default=True)
+@click.option('--heatmap', required=False, default=True,
+              show_default=True)
+@click.option('--paired_heatmap', required=False, default=False,
+              show_default=True)
+@click.option('--title', required=False, default='Mixing Proportions',
+              show_default=True)
+@click.option('--color', required=False, default='viridis',
+              show_default=True)
+@click.option('--unknowns', required=False, default=True,
+              show_default=True)
+@click.option('--transpose', required=False, default=False,
+              show_default=True)
 
-    def filter_list(inds, factor): return [ind for ind in list(inds)
-                                           if ind not in factor]
-    # concat each sink-source (dropping sources with same name as sink)
-    fas_merged = pd.concat({sink: source.reindex(filter_list(source.index,
-                                                             sink))
-                            for sink, source in zip(mpm.columns, fas)})
-    # if loo is True then columns are source-source
-    if loo:
-        columns_ = ['Source_one', 'Source_two']
-    # if loo is False then columns as sink-source
-    else:
-        columns_ = ['Sink', 'Source']
-    # make the index map and mapping in the same step
-    ss_map = {'sample%i' % i: list(map(str, v))
-              for i, v in enumerate(fas_merged.index.tolist())}
-    ss_map = pd.DataFrame(ss_map, columns_).T
-    ss_map.index.name = 'sampleid'
-    # output for QIIME2
-    fas_merged.index = ss_map.index
-    fas_merged = Table(fas_merged.T.values,
-                       fas_merged.T.index,
-                       fas_merged.T.columns)
-    # this is because QIIME will only
-    # support these for now
-    # in the future we will work
-    # on supporting collections (i.e. fas)
-    return mpm, mps, fas_merged, ss_map
-
-
-def gibbs_helper(feature_table: Table,
-                 sample_metadata: pd.DataFrame,
-                 loo: bool,
-                 jobs: int,
-                 alpha1: float,
-                 alpha2: float,
-                 beta: float,
-                 source_rarefaction_depth: int,
-                 sink_rarefaction_depth: int,
-                 restarts: int,
-                 draws_per_restart: int,
-                 burnin: int,
-                 delay: int,
-                 per_sink_feature_assignments: bool,
-                 sample_with_replacement: bool,
-                 source_sink_column: str,
-                 source_column_value: str,
-                 sink_column_value: str,
-                 source_category_column: str) -> (pd.DataFrame,
-                                                  pd.DataFrame,
-                                                  list):
+def gibbs_cli(table_fp, mapping_fp, output_dir, loo, jobs, alpha1, alpha2,
+              beta, source_rarefaction_depth, sink_rarefaction_depth, restarts,
+              draws_per_restart, burnin, delay, per_sink_feature_assignments,
+              sample_with_replacement, source_sink_column,
+              source_column_value, sink_column_value,
+              source_category_column, diagnostics, limit, title,
+              stacked_bar, heatmap, paired_heatmap, color, unknowns,
+              transpose):
     '''Gibb's sampler for Bayesian estimation of microbial sample sources.
 
-    This function is a helper that applies to both the click and QIIME2
-    command line functionality.
+    For details, see the project README file.
     '''
+    # Create results directory. Click has already checked if it exists, and
+    # failed if so.
+    os.mkdir(output_dir)
+
+    # Load the metadata file and feature table.
+    sample_metadata = parse_sample_metadata(open(mapping_fp, 'U'))
+    feature_table = biom_to_df(load_table(table_fp))
 
     # Do high level check on feature data.
     feature_table = validate_gibbs_input(feature_table)
@@ -186,15 +236,94 @@ def gibbs_helper(feature_table: Table,
         sinks = None
 
     # Run the computations.
-    mpm, mps, fas = _gibbs(csources, sinks, alpha1, alpha2, beta, restarts,
-                           draws_per_restart, burnin, delay, jobs,
-                           create_feature_tables=per_sink_feature_assignments)
-    # number of returns chnages based on flag
-    # this was refactored for QIIME2
-    # transpose to follow convention
-    # rows are features (i.e. taxa)
-    # columns are samples
+    mpm, mps, fas = gibbs(csources, sinks, alpha1, alpha2, beta, restarts,
+                          draws_per_restart, burnin, delay, jobs,
+                          create_feature_tables=per_sink_feature_assignments)
+
+    # Write results.
+    mpm.to_csv(os.path.join(output_dir, 'mixing_proportions.txt'), sep='\t')
+    mps.to_csv(os.path.join(output_dir, 'mixing_proportions_stds.txt'),
+               sep='\t')
     if per_sink_feature_assignments:
-        return mpm.T, mps.T, fas
-    else:
-        return mpm.T, mps.T
+        for sink, fa in zip(mpm.index, fas):
+            fa.to_csv(os.path.join(output_dir, sink + '.feature_table.txt'),
+                      sep='\t')
+
+    # Plot contributions.
+    graphs = ST_graphs(mpm, output_dir, title=title, color=color)
+    if heatmap:
+        graphs.ST_heatmap()
+        if not unknowns:
+            graphs.ST_heatmap(unknowns=False)
+    if paired_heatmap:
+        graphs.ST_paired_heatmap()
+        if not unknowns:
+            graphs.ST_paired_heatmap(unknowns=False)
+            graphs.ST_paired_heatmap(unknowns=False, normalized=True)
+        if transpose:
+            graphs.ST_paired_heatmap(unknowns=False, normalized=True,
+                                     transpose=True)
+    if stacked_bar:
+        graphs.ST_Stacked_bar()
+        if not unknowns:
+            graphs.ST_Stacked_bar(unknowns=False)
+
+    fig, ax = plot_heatmap(mpm)
+    fig.savefig(os.path.join(output_dir, 'mixing_proportions.pdf'), dpi=300)
+    
+    #modified: testing stats output
+    if diagnostics:
+        os.mkdir(output_dir+'diagnostics')
+        data = np.load('envcounts.npy')
+        sink_ids = np.load('sink_ids.npy')
+        source_ids = np.load('source_ids.npy')
+        file_path = output_dir+'diagnostics'
+        
+        source_ids = np.append(source_ids, ['unknown'])
+        df = pandas.DataFrame(source_ids)
+        sink_index = -1
+        for array in data:
+            sink_df = []
+            sink_index += 1
+            sink_id = sink_ids[sink_index]
+            source_index = -1
+        
+            for sources in source_ids:
+                source_index += 1
+                source_array = array[:,source_index]
+                split_array = np.array_split(source_array, draws_per_restart)
+                plt.figure(figsize=(8,6), dpi=300), plt.title(sink_id, fontsize = (16))
+                
+                flagged = []
+                for splits in split_array:
+                    data_sum = np.cumsum(splits)
+                    restart_num = np.size(data_sum)
+                    vector = np.linspace(1, restart_num, restart_num)
+                    rolling = np.true_divide(data_sum, vector)
+                    
+                    scalar = [(endpoint*alpha1) for endpoint in rolling]
+                    line_average = np.average(scalar)
+                    line_average = np.round(line_average, decimals=4)
+                    flagged.append(line_average)
+                    plt.plot(scalar, label=line_average), plt.legend(), plt.ylabel(sources, fontsize=(16))
+                    
+                absolutes = [abs(chains) for chains in flagged]
+                difference = (max(absolutes)-min(absolutes))
+                sink_df.append(difference)
+                
+                if difference >= limit:
+                    file_name = sink_id+'_'+sources+'.png'
+                    plt.savefig(os.path.join(file_path, file_name))
+                else:
+                    pass
+                plt.close()
+        
+            sink_df = pandas.DataFrame(sink_df)
+            df[sink_id] = sink_df
+            df.columns.values[0] = ''
+            df.set_index('').T
+            df.to_csv(file_path+'/'+'table.txt', sep='\t', index=False)
+            
+    os.remove('envcounts.npy')
+    os.remove('sink_ids.npy')
+    os.remove('source_ids.npy')
